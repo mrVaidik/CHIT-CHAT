684dcd3982b584487d2ff3dff80933243d9c9f18
Vaidikpatil <vaidikpatil0@gmail.com>
2025-07-26 14:52:31 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,20 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
 });
 
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 await connectDB();
 
