9608d0f1f9ad52ec68d6be50facb9f441467d5e1
Vaidikpatil <vaidikpatil0@gmail.com>
2025-07-26 12:04:15 +0530

    final url

--- frontend/src/config/config.jsx
+++ frontend/src/config/config.jsx
@@ -1,3 +1,2 @@
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
-export const SOCKET_URL =
-  import.meta.env.VITE_SOCKET_URL || "http://localhost:3000";
+export const API_URL = import.meta.env.VITE_API_URL;
+export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
