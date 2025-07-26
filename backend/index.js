0efdd41097b336938a90385663498896e910ad3c
Vaidikpatil <vaidikpatil0@gmail.com>
2025-07-26 11:58:07 +0530

    final

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 connect();
 app.use(express.json());
-app.use(cors({ origin: process.env.CLIENT_ORIGIN }));
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/rooms", roomRoutes);
